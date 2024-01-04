d771ceeba6ba384c02ef377317a3f2644268c9dc
Jeff Ripke <jripke74@gmail.com>
2024-01-04 14:19:39 -0800

    add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import AddProduct from './components/admin/AddProduct.jsx';
+
+import './App.css';
+
+function App() {
+  return (
+    <AddProduct />
+  );
+}
+
+export default App;
---
+++ frontend/src/components/admin/AddProduct.jsx
@@ -0,0 +1,18 @@
+const AddProduct = function AddProduct() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="/">Shop</a></li>
+          <li><a href="/add-product">Add Product</a></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>My Products</h1>
+        <p>List of all the products.</p>
+      </main>
+    </header>
+  );
+}
+
+export default AddProduct;
