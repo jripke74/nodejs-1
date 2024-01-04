1fb8319af5ce8848093ab5afcda6daadf9737514
Jeff Ripke <jripke74@gmail.com>
2024-01-04 11:37:27 -0800

    add routes

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,15 @@
 const express = require("express");
+
 const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.use("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res) => {
-  res.send("<h1>Hello from Express!");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
